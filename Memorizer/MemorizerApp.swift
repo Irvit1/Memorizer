545f88f241449836eb4110cda66eb9d092a2a819
Irvit1 <irvit2002gupta@gmail.com>
2021-04-21 00:05:55 +0530

    Initial Commit

---
+++ Memorizer/MemorizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizerApp.swift
+//  Memorizer
+//
+//  Created by Irvit Gupta on 21/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MemorizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
